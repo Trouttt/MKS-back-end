e901e7e25c7cea34761b75fee122eb0f5ce6055c
Leonardo <desu987@hotmail.com>
2022-11-22 08:13:03 -0300

    first commit

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Account } from 'src/modules/accounts/entities/account.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+}
